5d6f3a878cb5d50b40bcdbe5ff16e9678e0a7dd0
SunLijiaoClarissa <181180136+SunLijiaoClarissa@users.noreply.github.com>
2025-09-29 22:22:22 +0800

    database config

--- RESTAPI-Server/event_db.js
+++ RESTAPI-Server/event_db.js
@@ -2,14 +2,17 @@
 const mysql = require("mysql2");
 const bodyParser = require("body-parser");
 
+// Import our db details
+var db = require("./db-details.js");
+
 // Create db Connection
 module.exports = {
     getConnection: ()=>{
         return mysql.createConnection({
-            host: "localhost",
-            user: "root",
-            password: "123456",
-            database: "charityevents_db"
+            host: db.host,
+            user: db.user,
+            password: db.password,
+            database: db.database 
         });
     }
 };
